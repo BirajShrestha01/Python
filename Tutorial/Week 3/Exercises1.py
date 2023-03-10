8d143fa9398ebba4fe585bace76640df3df9e327
Biraj Shrestha <birajshrestha2007.com>
2023-03-10 08:12:04 +0545

    added comment

--- Tutorial/Week 3/Exercises1.py
+++ Tutorial/Week 3/Exercises1.py
@@ -1,30 +1,42 @@
 # WAP to develop a simple calculator, defining each function for each calculation. [Include a welcome message]
 
+# Function for welcome message using docstring
 def welcome():
     """Welcome to my simple calculator"""
+
+# Function for addition
 
 
 def add(a, b):
     return a+b
 
+# Function for subtraction
+
 
 def subtract(a, b):
     return a-b
 
+# Function for multiplication
+
 
 def multiply(a, b):
     return a*b
+
+# Function for division
 
 
 def divide(a, b):
     return a/b
 
 
+# Printing welcome message
 print(welcome.__doc__)
 
+# Taking numbers from user
 x = int(input("Enter number 1:"))
 y = int(input("Enter number 2:"))
 
+# Calling functions for simple calculations
 print("Sum:", add(x, y))
 print("Difference:", subtract(x, y))
 print("Product:", multiply(x, y))
