f2ac090f2438edb4209ac8f7010812c1d7612349
Biraj Shrestha <birajshrestha2007.com>
2023-03-10 07:35:39 +0545

    added new function

--- Tutorial/Week 3/functions.py
+++ Tutorial/Week 3/functions.py
@@ -1,12 +1,21 @@
-def name():
-    print("Biraj Shrestha")
+# def name():
+#     print("Biraj Shrestha")
+#
+#
+# name()
+#
+#
+# def age(n):
+#     print("My age is", n)
+#
+#
+# age(18)
 
 
-name()
+def add(a, b):
+    sum = a+b
+    return sum
 
 
-def age(n):
-    print("My age is", n)
-
-
-age(18)
+a = add(100, 200)
+print("The sum of numbers is", a)
