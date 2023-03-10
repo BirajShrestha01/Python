fb1952c993401c7031ce48e724ceed2e87411b9d
Biraj Shrestha <birajshrestha2007.com>
2023-03-10 07:49:46 +0545

    added docstring

--- Tutorial/Week 3/functions.py
+++ Tutorial/Week 3/functions.py
@@ -1,35 +1,42 @@
-# Function with non return type
-def name():
-    print("Biraj Shrestha")
+# # Function with non return type
+# def name():
+#     print("Biraj Shrestha")
 
 
-name()
+# name()
 
 
-def age(n):
-    print("My age is", n)
+# def age(n):
+#     print("My age is", n)
 
 
-age(18)
+# age(18)
 
-# Function to add
-
-
-def add(a, b):
-    sum = a+b
-    return sum
+# # Function to add
 
 
-a = add(100, 200)
-print("The sum of numbers is", a)
-
-# Function to multipy
-
-
-def multiply(a, b):
-    product = a*b
-    return product
+# def add(a, b):
+#     sum = a+b
+#     return sum
 
 
-b = multiply(100, 200)
-print("The product of numbers is", b)
+# a = add(100, 200)
+# print("The sum of numbers is", a)
+
+# # Function to multipy
+
+
+# def multiply(a, b):
+#     product = a*b
+#     return product
+
+
+# b = multiply(100, 200)
+# print("The product of numbers is", b)
+
+def greet(name):
+    """Hello everyone welcome to my python code"""
+    print(f"Hello,{name}!")
+
+
+print(greet.__doc__)
